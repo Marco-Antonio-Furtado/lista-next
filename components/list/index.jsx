039a02c9a47b7fb5a1658969a74b4d21551126ce
Marco Antônio Furtado <marcofurt@gmail.com>
2021-10-26 18:32:57 -0300

    Setando o inputdata
    Toda vez que uma tecla é digitada, o inputData é atualizado e recebe o valor da dita tecla

--- components/list/index.jsx
+++ components/list/index.jsx
@@ -1,12 +1,18 @@
 import styles from "./index.module.css"
 import Button from "../button"
 import Task from "../task"
+import { useState } from "react"
 
 export default  function List(){
+    const [inputData, setInputData] = useState("")
+    function handleInputData(e)
+    {setInputData(e.target.value)}
+    console.log(inputData)
+
     return(
     <div className={styles.wrapper}>
         <header className={styles.header}>
-            <input className={styles.input}/>
+            <input className={styles.input} onChange={handleInputData}/>
             <Button className={styles.btnAdd} />
         </header>
         <main className={styles.main}>

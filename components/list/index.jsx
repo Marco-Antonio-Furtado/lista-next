1446ca01da934c5714243469a3fc98ef018db581
Marco Antônio Furtado <marcofurt@gmail.com>
2021-10-28 21:27:53 -0300

    Adicionando tarefas
    Problema no valor do input. não consigo fazer com que ele se apague ao clicar no botão

--- components/list/index.jsx
+++ components/list/index.jsx
@@ -5,18 +5,47 @@ import { useState } from "react"
 
 export default  function List(){
     const [inputData, setInputData] = useState("")
+    
     function handleInputData(e)
     {setInputData(e.target.value)}
-    console.log(inputData)
 
-    return(
+
+    const [list, setList] = useState(
+        
+        [
+            {
+                id: 1,
+                title: "Something",
+                completed: true,
+             },
+             {
+                 id: 2,
+                 title: "Other thing",
+                 completed: false
+             },
+         ]
+     )
+     function addtask(){
+         const newList = [...list, 
+             {
+                 id:3,
+                 title:inputData,
+                 completed:true,
+             }    
+         ]
+         setList(newList)
+         setInputData("")
+     } 
+
+        
+        return(
     <div className={styles.wrapper}>
         <header className={styles.header}>
             <input className={styles.input} onChange={handleInputData}/>
             <Button className={styles.btnAdd} onClick={addtask}>Adicionar</Button>
         </header>
         <main className={styles.main}>
-            {[1, 2, 3].map((n, i)=><Task key={i} />)}
+            {list.map((task, i)=><Task key={i} task={task} />)}
         </main>
     </div>
     )

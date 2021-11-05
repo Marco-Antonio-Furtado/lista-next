023611d40ae3bb591cdb004b99fd148367376364
Marco Antônio Furtado <marcofurt@gmail.com>
2021-11-05 15:53:25 -0300

    estilizando

--- components/task/index.jsx
+++ components/task/index.jsx
@@ -5,7 +5,7 @@ export default function Task({ task, handleTaskClick, taskDelete }){
         <div className={styles.task}
         style={task.completed ? { borderLeft: "5px solid chartreuse" } : {}}>
             <p className={styles.taskTitle}  onClick={()=>handleTaskClick(task)} >{task.title}</p>
-            <p onClick={() =>taskDelete(task.id)}>x</p>
+            <p className={styles.btnDelete} onClick={() =>taskDelete(task.id)}>X</p>
         </div>
     )
 }

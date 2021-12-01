27a16e6787175bd55cf7fdf0847b05c7fefc32bc
Marco Antônio Furtado <marcofurt@gmail.com>
2021-12-01 19:13:40 -0300

    criando o componente AddTitle
    este componente retorna uma div expansiva com um botao que, ao ser clicado, se transforma num input que receberá o nome de uma nova lista

---
+++ src/components/addTitle/index.jsx
@@ -0,0 +1,74 @@
+import { useState, useRef } from "react";
+import styles from "./index.module.css";
+import List from "../list";
+import { v4 as uuid } from "uuid";
+import Button from "@mui/material/Button";
+
+export default function AddTitle({ kanban, setKanban }) {
+  const [inputData, setInputData] = useState("");
+  const [close, setClose] = useState(true);
+
+  function handleInputData(e) {
+    setInputData(e.target.value);
+  }
+
+  function newList() {
+    const newKanban = [
+      ...kanban,
+      {
+        id: uuid(),
+        name: inputData,
+        tasks: [],
+      },
+    ];
+    setKanban(newKanban);
+    window.localStorage.setItem("kanban", JSON.stringify(kanban));
+    setInputData("");
+  }
+
+  function handleEnter(e) {
+    if (e.keyCode == 13) {
+      newList();
+    }
+  }
+
+  function handleBlur(e) {
+    setTimeout(() => {
+      setClose(!close);
+    }, 100);
+  }
+
+  return (
+    <div>
+      <div className={close ? styles.container : styles.containerMod}>
+        {close ? (
+          <div>
+            <button
+              className={styles.btnAddList}
+              onClick={() => setClose(!close)}
+            >
+              + Add list
+            </button>
+          </div>
+        ) : (
+          <div className={styles.box}>
+            <input
+              className={styles.iptAddList}
+              onChange={handleInputData}
+              onKeyUp={(e) => handleEnter(e)}
+              onBlur={(e) => handleBlur(e)}
+              value={inputData}
+              autoFocus
+            />
+            <div className={styles.btnMUI}>
+              <Button variant="contained" onClick={newList}>
+                + Add
+              </Button>
+              <Button onClick={() => setClose(!close)}>X</Button>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}

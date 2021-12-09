25d534bf32ad9001a2285535b98fd6e61159511c
Marco Antônio Furtado <marcofurt@gmail.com>
2021-12-09 15:54:03 -0300

    Estudando o comportamento do localStorage

--- src/components/kanban/index.jsx
+++ src/components/kanban/index.jsx
@@ -8,10 +8,10 @@ import { useKanban } from "../../context/Kanban";
 export default function Kanban() {
   const { kanban, setKanban } = useKanban();
   useEffect(() => {
-    const kanbanFromStorage = window.localStorage.getItem("kanban");
-    const kanbanFromStorageArr = JSON.parse(kanbanFromStorage);
-
-    setKanban((state) => kanbanFromStorageArr);
+    // const kanbanFromStorage = window.localStorage.getItem("kanban");
+    // const kanbanFromStorageArr = JSON.parse(kanbanFromStorage);
+    // setKanban((state) => kanbanFromStorageArr);
+    window.localStorage.setItem("kanban", JSON.stringify(kanban));
   }, []);
 
   return (
